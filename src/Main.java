0005ee596ab864c695636ead3255367db81b768d
KolesnikVV <Voltpluc@ya.ru>
2023-02-13 19:58:56 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+    public static void task1(){
+        System.out.println("Задание 1");
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний.");
+        } else  {
+            System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
 
-        System.out.println("Hello world!");
     }
 }
