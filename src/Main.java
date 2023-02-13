b592f611ee0d5e5a9d939e8c7568a4327d12b542
KolesnikVV <Voltpluc@ya.ru>
2023-02-13 20:05:44 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1(){
         System.out.println("Задание 1");
@@ -9,6 +10,15 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + " то он совершеннолетний.");
         } else  {
             System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
+    }
+    public static void task2(){
+        System.out.println("Задание2");
+        int temperature = -3;
+        if (temperature >= 5) {
+            System.out.println("На улице " + temperature + " градусов ,можно идти без шапки.");
+        } else  {
+            System.out.println("На улице " + temperature + " градусов ,нужно надеть шапку!");
         }
 
     }
